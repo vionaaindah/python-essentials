b34c2db00bf0367859ef0e244b1409e5511f43d6
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2021-07-23 12:27:56 +0800

    Create 3.2.1.14.py

---
+++ module-3/3.2.1.14.py
@@ -0,0 +1,14 @@
+blocks = int(input("Enter the number of blocks: "))
+
+# Write your code here.
+height = 0
+while True:
+    height += 1
+    blocks -= height
+    if blocks == 0:
+        break
+    if blocks < 0:
+        height -= 1
+        break
+
+print("The height of the pyramid:", height)
