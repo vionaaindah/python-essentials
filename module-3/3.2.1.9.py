a537f23067422821a12634f1bfbf5db98458bc59
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2021-07-23 12:01:11 +0800

    Create 3.2.1.9.py

---
+++ module-3/3.2.1.9.py
@@ -0,0 +1,6 @@
+word = input("Enter the word: ")
+while True:
+    if word == "chupacabra":
+        print("You've succesfully left the loop.")
+        break
+    word = input("Enter another word: ")
