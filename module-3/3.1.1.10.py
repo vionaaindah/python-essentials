578fa8b0fd430b84ba35fd296944731e35ad0947
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2021-07-23 11:02:00 +0800

    Create 3.1.1.10.py

---
+++ module-3/3.1.1.10.py
@@ -0,0 +1,8 @@
+word = input("Sample input: ")
+
+if word == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif word== "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not, " + word + "!")
