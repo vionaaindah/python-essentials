6bffd6f002aa42bce6756a38068dfee4c1244e77
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2021-07-23 11:04:23 +0800

    Create 3.1.1.11.py

---
+++ module-3/3.1.1.11.py
@@ -0,0 +1,13 @@
+income = float(input("Enter the annual income: "))
+
+# Write your code here.
+if income <= 85528:
+    tax = (income * 0.18) - 556 - 0.02
+elif income > 85528:
+    tax = (14839 + 0.02) + 0.32 * (income - 85528)
+
+if tax < 0:
+    tax = 0
+
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
