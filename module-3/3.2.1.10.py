7747590fb5e9767d068f0cd016e7d83d04c94db7
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2021-07-29 11:20:06 +0800

    Update 3.2.1.10.py

--- module-3/3.2.1.10.py
+++ module-3/3.2.1.10.py
@@ -2,18 +2,11 @@
 # and assign it to the user_word variable.
 user_word = input("Enter a word: ")
 user_word = user_word.upper()
+vowels = "AIUEO"
 
 for letter in user_word:
-    # Complete the body of the for loop.
-    if letter == "A":
-        continue
-    elif letter == "I":
-        continue
-    elif letter == "U":
-        continue
-    elif letter == "E":
-        continue
-    elif letter == "O":
+    # Complete the body of the loop.
+    if letter in vowels:
         continue
     else:
         print(letter)
