8c7c7744a064c46f3153e2a3a9331b58bd75c9d3
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2021-07-29 11:19:19 +0800

    Update 3.2.1.11.py

--- module-3/3.2.1.11.py
+++ module-3/3.2.1.11.py
@@ -4,18 +4,11 @@ word_without_vowels = ""
 # and assign it to the user_word variable.
 user_word = input("Enter a word: ")
 user_word = user_word.upper()
+vowels = "AIUEO"
 
 for letter in user_word:
     # Complete the body of the loop.
-    if letter == "A":
-        continue
-    elif letter == "I":
-        continue
-    elif letter == "U":
-        continue
-    elif letter == "E":
-        continue
-    elif letter == "O":
+    if letter in vowels:
         continue
     else:
         word_without_vowels += letter
