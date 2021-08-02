cdd1f9f0d6bcda5624c25bf5411eeb74cddab8c1
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2021-08-02 15:06:13 +0800

    Create 4.3.1.15.py

---
+++ module-8/4.3.1.15.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from os import strerror
+
+srcname = input("Enter the source file name: ")
+
+try:
+    text = open(srcname, 'rt')
+    char = ''
+    for ch in text:
+        if ch.isalpha():
+            ch = ch.lower()
+            char = char + ch
+        else:
+            continue
+    c = Counter(char)
+
+    for letter, repetitions in c.items():
+        print(letter, "->", repetitions)
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
